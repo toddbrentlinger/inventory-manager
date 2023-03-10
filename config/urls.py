b0e6f2770c06099279faefabdd938ab648c5922c
toddbrentlinger <toddbrentlinger@gmail.com>
2023-03-09 20:05:09 -0800

    Add login/logout functionality

--- config/urls.py
+++ config/urls.py
@@ -19,8 +19,9 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
     path('', include('users.urls')),
+    path('accounts/', include('django.contrib.auth.urls')),
+    path('admin/', admin.site.urls),
     path('inventories/', include('inventories.urls')),
     path('items/', include('items.urls')),
 ]
