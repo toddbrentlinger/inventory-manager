b5c50ebd3a7aaef0cac6184fc44dbf17c7a64462
toddbrentlinger <toddbrentlinger@gmail.com>
2023-03-13 18:38:11 -0700

    Add login requirement to view inventories or items

--- inventories/views.py
+++ inventories/views.py
@@ -1,15 +1,18 @@
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.core.exceptions import PermissionDenied
 from django.shortcuts import render, get_object_or_404
 from django.views import generic
 from .models import Inventory, InventoryGroup
 
-class InventoryListView(generic.ListView):
+class InventoryListView(LoginRequiredMixin, generic.ListView):
     model = Inventory
     paginate_by = 25
 
-class InventoryDetailView(generic.DetailView):
+class InventoryDetailView(LoginRequiredMixin, generic.DetailView):
     model = Inventory
 
+@login_required
 def inventory_detail_view(request, pk):
     inventory = get_object_or_404(Inventory, pk=pk)
 
@@ -24,9 +27,10 @@ def inventory_detail_view(request, pk):
 
     return render(request, 'inventories/inventory_detail.html', context=context)
 
-class InventoryGroupDetailView(generic.DetailView):
+class InventoryGroupDetailView(LoginRequiredMixin, generic.DetailView):
     model = InventoryGroup
 
+@login_required
 def inventory_group_detail_view(request, pk):
     inventory_group = get_object_or_404(InventoryGroup, pk=pk)
 
--- items/views.py
+++ items/views.py
@@ -1,14 +1,17 @@
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.shortcuts import render, get_object_or_404
 from django.views import generic
 from .models import Item
 
-class ItemListView(generic.ListView):
+class ItemListView(LoginRequiredMixin, generic.ListView):
     model = Item
     paginate_by = 25
 
-class ItemDetailView(generic.DetailView):
+class ItemDetailView(LoginRequiredMixin, generic.DetailView):
     model = Item
 
+@login_required
 def item_detail_view(request, pk):
     item = get_object_or_404(Item, pk=pk)
 
