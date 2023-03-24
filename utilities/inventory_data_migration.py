35fb00e51d93cef070c6983c352e8fb34d4d8758
toddbrentlinger <toddbrentlinger@gmail.com>
2023-03-23 19:10:12 -0700

    Improve data migration script
    Adds list of brands that can be created and assigned to items.
    Adds image to items from urls in spreadsheet using request python
    library.

--- utilities/inventory_data_migration.py
+++ utilities/inventory_data_migration.py
@@ -1,8 +1,11 @@
 from decouple import config
 from django.conf import settings
+from django.core.files.base import ContentFile
 from pandas_ods_reader import read_ods
 
 import pandas as pd
+import re
+import requests
 
 class Models:
     def __init__(self, apps):
@@ -40,6 +43,35 @@ def add_model_inst_list_to_field(m2m_field, model_inst_list):
 
 def initialize_database(apps, schema_editor):
     models = Models(apps)
+    brands = [
+        'Anvil',
+        'Bessey',
+        'Bosch',
+        'Buck Bros',
+        'Comoware',
+        'Craftsman',
+        'Dewalt',
+        'Diablo',
+        'Empire',
+        'Estwing',
+        'Firm Grip',
+        'Flexzilla',
+        'Freud',
+        'Goodjob',
+        'Husky',
+        'Irwin Tools',
+        'Jastind',
+        'Klein Tools',
+        'Metabo',
+        'Milescraft',
+        'Narex',
+        'Pony',
+        'Ruitool',
+        'Stanley',
+        'Sungator',
+        'Swanson',
+        'Yoselin',
+    ]
 
     # Create two test users to add items to their inventory and save them to the database
     user1 = models.User.objects.create_user('user1', 'user1@mail.com', 'password1')
@@ -76,6 +108,14 @@ def initialize_database(apps, schema_editor):
         # Create Item object
         item = models.Item()
 
+        # Dictionary to hold model instances for ManyToManyFields.
+        # Key is field name and value is list of model instances.
+        # After other fields in Item instance are set and it's saved to database,
+        # can then add to the actual ManyToManyFields.
+        item_m2m_instances_dict = {
+            'images': [],
+        }
+
         # Inventory (ForeignKey)
         # Add tool Item to first or second Users Inventory
         item.inventory = inventory1 if index < user1_number_items else inventory2
@@ -87,7 +127,15 @@ def initialize_database(apps, schema_editor):
         item.model_number = row[2] if row[2] is not None else ''
 
         # Serial Number (Char)
-        # Brand (ForeignKey)
+
+        # Brand (ForeignKey) - Check with brands array for known brands
+        for brand in brands:
+            if re.search(f'(?<![a-z0-9]){ brand }(?![a-z0-9])', row[1], re.I):
+                # Get or create Brand model instance
+                item.brand = models.Brand.objects.get_or_create(
+                    name=brand
+                )
+
         # Description (Text)
 
         # Price (Integer) - number of American cents
@@ -96,15 +144,15 @@ def initialize_database(apps, schema_editor):
             item.price = row[3] * 100
 
         # Purchase Date - (Date)
-        # Images - (ManyToMany)
+        # TODO: Keep variable of last purchase date for items with same 
+        # purchase date in combined data cells
 
-        # Dictionary to hold model instances for ManyToManyFields.
-        # Key is field name and value is list of model instances.
-        # After other fields in Item instance are set and it's saved to database,
-        # can then add to the actual ManyToManyFields.
-        item_m2m_instances_dict = {
-            'images': [],
-        }
+        # Images - (ManyToMany)
+        # TODO: Use requests library to access image from url
+        response = requests.get(row[4])
+        if response.status_code == 200:
+            image_content = ContentFile(response.content)
+            item_m2m_instances_dict['images'].append(image_content)
 
         # Save Item object to database
         item.save()
@@ -132,6 +180,16 @@ def main():
 
     # Print Pandas.DataFrame object containing ODS file spreadsheet
     print(df)
+
+    # https://developer-docs.amazon.com/amazon-business/docs/product-search-api-v1-reference
+    params = {
+        'keywords': 'Dewalt 20V MAX* ½ in. Cordless Drill/Driver'
+    }
+    response = requests.get(
+        "https://na.business-api.amazon.com/products/2020-08-26/products",
+        params=params
+    )
+    print(response.json())
 
 if __name__ == '__main__':
     main()
